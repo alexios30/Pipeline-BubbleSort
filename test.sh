c30fd720ea582e6f85726ff173dbd1496c886902
Alexios <alexischampault.travail@gmail.com>
2025-10-15 19:31:05 +0200

    Change test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-./bubblesort > output.txt
+# Fournir des entrées au programme : 1 (entiers) puis 1 (croissant) puis 3 (quitter)
+echo -e "1\n1\n3" | ./bubblesort > output.txt
 
 if grep -q "11 12 22 25 34 64 90" output.txt; then
     echo "Test réussi ✅"
